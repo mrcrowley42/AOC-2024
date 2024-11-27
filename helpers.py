46c4cd34ea88fe8eeba90fb5810acae4b24c81c0
mrcrowley42 <mattcrowley42@me.com>
2024-11-27 21:17:01 +1100

    add helpers

---
+++ helpers.py
@@ -0,0 +1,11 @@
+class Vector2:
+    def __init__(self,r,c):
+        self.r, self.c = r, c
+        
+    def __add__(self, other):
+        return Vector2(self.r + other.r, self.c + other.c)
+    
+    def location(self):
+        return (self.r, self.c)   
+
+directions = {"L": Vector2(0, -1), "R": Vector2(0, 1), "D": Vector2(-1, 0), "U": Vector2(1, 0)}
