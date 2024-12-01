ad99eadea617df8d1e672f60323d0931a3b70c37
mrcrowley42 <mattcrowley42@me.com>
2024-12-01 16:24:10 +1100

    day 1

--- day1.py
+++ day1.py
@@ -1,2 +1,16 @@
+from collections import Counter
+
 with open("inputs/day1_input.txt") as file:
     input = file.read().splitlines()
+
+numbers = [list(map(int, line.split())) for line in input]
+left = sorted([line[0] for line in numbers])
+right = sorted([line[1] for line in numbers])
+diffs = [abs(left[i] - right[i]) for i in range(len(numbers))]
+# Part 1
+print(sum(diffs))
+
+counts = Counter(right)
+sim_scores = [id * count for id, count in counts.items() if id in left]
+# Part 2 
+print(sum(sim_scores))
