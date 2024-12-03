b6c4a714d1963674723f5d67fc50307d90e62450
mrcrowley42 <mattcrowley42@me.com>
2024-12-03 17:51:16 +1100

    regex to the rescue

--- day2.py
+++ day2.py
@@ -4,52 +4,23 @@ with open("inputs/day2_input.txt") as file:
     
 numbers = [list(map(int, line.split())) for line in input]
 
-def fast():
-    count = 0
-    for x_line in numbers:
-        for i in range(len(x_line)):
-            line = x_line[:i]+x_line[i+1:]
-            if line == sorted(line) or line == sorted(line, reverse=True):
-                if all([ 0 < (abs(line[i] - line[i-1])) < 4 for i in range(1, len(line))]):
-                    count += 1
-                    break
-    return count
-                
-        
-        
-    
-
 def ascending(line):
-    for i in range(1, len(line)):
-        if line[i] <= line[i-1]:
-            return False
-    return True
+    return all(line[i] > line[i-1] for i in range(1, len(line)))
 
 
 def descending(line):
-    for i in range(1, len(line)):
-        if line[i] >= line[i-1]:
-            return False
-    return True
+    return all(line[i] < line[i-1] for i in range(1, len(line)))
 
 
 def is_safe(line):
-    if not ascending(line) and not descending(line):
+    if not (ascending(line) or descending(line)):
         return 0
-    
-    for i in range(1, len(line)):
-        if not 0 < (abs(line[i] - line[i-1])) < 4:
-            return 0
-    return 1
-   
-            
+    return 1 if all(0 < (abs(line[i] - line[i-1])) < 4 for i in range(1, len(line))) else 0
+
+
 def any_safe(line):
-    for i in range(len(line)):
-        new_line = line[:i]+line[i+1:]
-        if is_safe(new_line):
-            return 1
-    return 0
+    return 1 if any(is_safe(line[:i]+line[i+1:]) for i in range(len(line))) else 0
 
 
-print((sum([any_safe(line) for line in numbers])))
-print(fast())
+print((sum(is_safe(line) for line in numbers)))
+print((sum(any_safe(line) for line in numbers)))
--- day3.py
+++ day3.py
@@ -1,2 +1,23 @@
+import re
+from math import prod
 with open("inputs/day3_input.txt") as file:
-    input = file.read().splitlines()
+    input = file.read()
+
+p1_total = 0
+p2_total = 0
+enabled = True
+matches = re.findall(r"mul\(\d*,\d*\)|do\(\)|don\'t\(\)", input)
+
+for match in matches:
+    if match == "do()":
+        enabled = True
+    elif match == "don't()":
+        enabled = False
+    else:
+        nums = re.findall(r"\d+", match)
+        if enabled:
+            p2_total += prod(list(map(int,nums)))
+        p1_total += prod(list(map(int,nums)))
+        
+print(p1_total)
+print(p2_total)
