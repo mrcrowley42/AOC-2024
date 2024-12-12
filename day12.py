1f9bda4382b186c89f0a0cbf12eaa31709155d07
mrcrowley42 <mattcrowley42@me.com>
2024-12-12 19:36:15 +1100

    day 12 part 2 idk

--- day12.py
+++ day12.py
@@ -1,2 +1,49 @@
+from time import time
+
 with open("inputs/day12_input.txt") as file:
-    input = file.read().splitlines()
+    input = [list(line) for line in file.read().splitlines()]
+
+start_t = time()
+
+
+positions_to_check = {(r, c) for r, row in enumerate(input) for c, value in enumerate(row)}
+offsets = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+
+def get_score(location, grid):
+    start_value = grid[location[0]][location[1]]
+    locations = [location]
+    visited = {(location[0], location[1])}
+    edges = 0
+    while locations:
+        r, c = locations.pop()
+        for dx, dy in offsets:
+            r2 = r + dx
+            c2 = c + dy
+            if not ( 0 <= r2 < len(grid) and 0 <= c2 < len(grid[0])):
+                edges += 1
+                continue
+            if grid[r2][c2] != start_value:
+                edges += 1
+                continue
+            if (r2, c2) not in visited:
+                locations.append([r2, c2])
+
+            visited.add((r2, c2))
+
+    for location in visited:
+        if location in positions_to_check:
+            positions_to_check.remove(location)
+
+    # return {start_value: (len(visited), edges)}
+    return len(visited) * edges
+
+total = 0
+
+while positions_to_check:
+    location = positions_to_check.pop()
+    result = get_score(location, input)
+    total += result
+
+print(total)
+print(time() - start_t)
