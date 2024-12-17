9f1269bbb9429dc555706612880497ad2ec9d215
mrcrowley42 <mattcrowley42@me.com>
2024-12-17 17:34:51 +1100

    17

--- day16.py
+++ day16.py
@@ -1,2 +1,33 @@
+import heapq 
+
 with open("inputs/day16_input.txt") as file:
-    input = file.read().splitlines()
+    maze = [list(line) for line in file.read().splitlines()]
+
+direction_map = {0: (-1, 0), 90: (0, 1), 180: (1, 0), 270: (0, -1)}
+
+def get_neighbours(r, c, direction):
+    neighbours = []
+    for deg, offset in direction_map.items():
+        dy, dx = offset
+        r2, c2 = r + dy, c + dx
+        if maze[r2][c2] != '#':
+            neighbours.append(((r2, c2), deg))
+    return neighbours
+
+for r, row in enumerate(maze):
+    for c, value in enumerate(row):
+        if value == 'S':
+            start = (r, c)
+        if value == 'E':
+            end = (r, c)
+
+
+# def search(start, end, maze):
+direction = 90
+r, c = start
+visited = {(r, c, direction)}
+pq = [(0, r, c, direction)]
+while pq:
+    cost, r, c, direction = heapq.heappop(pq)
+
+# print(search(start, end, maze))
--- day17.py
+++ day17.py
@@ -1,2 +1,56 @@
 with open("inputs/day17_input.txt") as file:
-    input = file.read().splitlines()
+    input = file.read().split('\n\n')
+
+steps = list(map(int, input[1].split(':')[1].split(',')))
+registers = [int(line.split(":")[1]) for line in input[0].splitlines()]
+registers = {key: value for key, value in zip(list('ABC'), registers)}
+
+def adv(operand):
+    registers["A"] = registers["A"] // (2**combo(operand))
+
+def bxl(operand):
+    registers["B"] = registers["B"] ^ operand
+
+def bst(operand):
+    registers["B"] = combo(operand) % 8
+
+def bxc(operand):
+    registers['B'] = registers['B'] ^ registers["C"]
+
+def bdv(operand):
+    registers["B"] = registers["A"] // (2**combo(operand))
+
+def cdv(operand):
+    registers["C"] = registers["A"] // (2**combo(operand))
+
+def combo(operand):
+    if operand < 4:
+        return operand
+    if operand == 4:
+        return registers['A']
+    if operand == 5:
+        return registers['B']
+    if operand == 6:
+        return registers['C']
+
+operation = {0: adv, 1: bxl, 2: bst, 4: bxc, 6: bdv, 7: cdv}  
+
+index = 0
+output = []
+while index < len(steps):
+    opcode = steps[index]
+    operand = steps[index + 1]
+    if opcode == 3:
+        if registers["A"] == 0:
+            pass
+        else:
+            index = operand
+            continue
+    elif opcode == 5:
+        value = combo(operand) % 8
+        output.append(value)
+    else:
+        operation[opcode](operand)
+    index += 2
+
+print(",".join(list(map(str, output))))
