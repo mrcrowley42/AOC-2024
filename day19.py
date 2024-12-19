a619d294dea8aafc777e5afb4a961707966315a4
mrcrowley42 <mattcrowley42@me.com>
2024-12-19 18:45:04 +1100

    19 part 1

--- day19.py
+++ day19.py
@@ -1,52 +1,25 @@
-from itertools import product as n_combinations
-from time import time
-
-
-start = time()
-
-
 with open("inputs/day19_input.txt") as file:
     avaiable, desired = [group.splitlines() for group in file.read().split('\n\n')]
 
 avaiable = avaiable[0].split(', ')
 
-# for design in desired:
-#     can_use = [pattern for pattern in avaiable if pattern in design]
-#     print(len(can_use))
-#     possible = 
-#     if design in possible:
 
-
-possible = 0
-wanted = set(desired)
-
-# for i in range(1, len(avaiable)):
-#     # print(i)
-#     if len(wanted) < 1:
-#         break
-#     combinations = ["".join(comb) for comb in list(n_combinations(avaiable, repeat=i))]
-#     remove = []
-#     for design in wanted:
-#         if design in combinations:
-#             possible += 1
-#             # print(design)
-#             remove.append(design)
-            
-#     for item in remove:
-#         wanted.remove(item)
-
-def check_possible(design):
+def check_possible(design:str):
     can_use = [pattern for pattern in avaiable if pattern in design]
-    for i in range(1, len(design)+1):
-        combinations = (n_combinations(can_use, repeat=i))
-        for combination in combinations:
-            # if design[0] != combination[0]:
-            #     continue
-            if design == "".join(combination):
-                return True
-    print(design)
+    pool = set([item for item in can_use if design.startswith(item)])
+    for _ in range(len(design)):
+        if design in pool:
+            return True
+        new_pool = []
+        for item in pool:
+            new_pool += [item + part for part in can_use if design.startswith((item + part))]
+        pool = set(new_pool)
+
     return False
     
+from time import time
+start = time()
+possible = 0
 for design in desired:
     if check_possible(design):
         possible += 1
