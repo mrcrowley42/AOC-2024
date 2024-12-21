9a80230249ce57b015e9c55c87cb76fa7ebc8d30
mrcrowley42 <mattcrowley42@me.com>
2024-12-21 16:35:31 +1100

    ???

--- day21.py
+++ day21.py
@@ -1,2 +1,14 @@
 with open("inputs/day21_input.txt") as file:
     input = file.read().splitlines()
+
+direction_map = {'^': (-1, 0), '>': (0, 1), 'v': (1, 0), '<': (0, -1)}
+
+keypad = [[7, 8, 9],
+          [4, 5, 6],
+          [3, 2, 1],
+          ['X', 0, 'A']]
+
+remote = [['x', '^', 'A'],
+          ['<', 'v', '>']]
+
+#what
