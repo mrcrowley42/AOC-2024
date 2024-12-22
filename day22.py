d1670b05736f44b6a83f1e1e9385d2425744d964
mrcrowley42 <mattcrowley42@me.com>
2024-12-22 16:32:29 +1100

    22

--- day22.py
+++ day22.py
@@ -1,2 +1,37 @@
 with open("inputs/day22_input.txt") as file:
-    input = file.read().splitlines()
+    input = list(map(int, file.read().splitlines()))
+
+def next_number(secret):
+    num = 64 * secret
+    secret = secret ^ num
+    secret = secret % 16777216
+    num = secret // 32
+    secret = secret ^ num
+    secret = secret % 16777216
+    num = secret * 2048
+    secret = secret ^ num
+    secret = secret % 16777216
+
+    return secret
+
+scores = 0
+
+for secret in input:
+    for i in range(2000):
+        secret = next_number(secret)
+    scores += secret
+    
+print(scores)
+
+# for line in input:
+#     secret = line
+#     prices = [int(str(secret)[-1]),]
+#     for i in range(10):
+#         secret = next_number(secret)
+#         price = int(str(secret)[-1])
+#         prices.append(price)
+#         print(prices)
+#         # print(secret)
+#     scores += secret
+    
+# print(scores)
